24208d50ca1f2524ed3920459f0559625b0ea922
Leo <leocsdev@gmail.com>
2023-05-01 00:29:56 +0800

    b002, added initial layout style.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,17 @@
+import ExpenseInfo from './ExpenseInfo';
+
 function Header() {
   const style = {
     fontSize: '1.5rem',
   };
 
   return (
-    <header>
-      <h1 className="text-center mt-4" style={style}>
+    <header className='header'>
+      <h1 className='text-center mt-4' style={style}>
         Expense Tracker
       </h1>
+
+      <ExpenseInfo />
     </header>
   );
 }
